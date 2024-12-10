42a40ef5e44454317c1f7d980fc8b29c090193ce
Milko Daskalov <mdaskalov@outlook.com>
2024-12-10 11:27:25 +0100

    use only xmlcoder

--- Package.swift
+++ Package.swift
@@ -3,33 +3,31 @@
 
 //
 //  Package.swift
-//  WSDL2Swift
+//  WSDLService
 //
-//  Created by Milko Daskalov on 07.11.24.
+//  Created by Milko Daskalov on 29.11.24.
 //
 
 import PackageDescription
 
 let package = Package(
-    name: "WSDL2Swift",
+    name: "WSDLService",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "WSDL2Swift",
-            targets: ["WSDL2Swift"]
+            name: "WSDLService",
+            targets: ["WSDLService"]
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/tadija/AEXML.git", from: "4.7.0"),
-        .package(url: "https://github.com/Thomvis/BrightFutures.git", from: "8.2.0"),
-        .package(url: "https://github.com/cezheng/Fuzi.git", from: "3.1.3"),
+        .package(url: "https://github.com/CoreOffice/XMLCoder.git", from: "0.17.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "WSDL2Swift",
-            dependencies: ["AEXML", "BrightFutures", "Fuzi"]
+            name: "WSDLService",
+            dependencies: ["XMLCoder"]
         )
     ]
 )
